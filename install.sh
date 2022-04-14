73b9b24ff34a408adf408500a73d7c5e4257db5d
emean1010 <zhou.emean@gmail.com>
2022-04-14 17:56:31 +0800

    无

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ sysctl -p
 lsmod | grep bbr
 
 # 安裝和更新 V2Ray
+cd /var/www/
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
 # 替换 v2ray 设置文件
 cp /var/www/v2/config.json /usr/local/etc/v2ray/config.json
