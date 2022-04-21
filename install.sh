df35d8538ca69c79f5674803c98dbe571fc211cb
emean1010 <zhou.emean@gmail.com>
2022-04-21 21:07:48 +0800

    【一键】

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 apt update
 
-apt install -y git zsh curl nginx
+apt install -y git zsh curl
 
 # 开启Google BBR （可选）显示 tcp_bbr 20480 0 这类结果则表示开启成功。
 echo 'net.core.default_qdisc=fq' | tee -a /etc/sysctl.conf
@@ -17,14 +17,7 @@ bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/
 # 替换 v2ray 设置文件
 cp /var/www/v2/config.json /usr/local/etc/v2ray/config.json
 
-# 删掉 nginx default 设置
-rm -f /etc/nginx/sites-enabled/default
-rm -f /etc/nginx/sites-available/default
-# 不要再 sites-available 里面放任何东西
-cp /var/www/v2/v2.nginx /etc/nginx/sites-enabled/v2
-
 service v2ray restart
-service nginx restart
 
 echo 'succsss'
 echo 'ip'
